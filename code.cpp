2935d11fd343d2cdd1292e11cd92aa4511613985
Abbas463 <abbashilal463@gmail.com>
2026-09-12 12:32:58 +0430

    Logical Operator OR

--- code.cpp
+++ code.cpp
@@ -3,9 +3,6 @@ using namespace std;
 
 int main () {
     
-    cout << (3 < 5) << endl;
-    cout << (3 != 5) << endl;
-    cout << (3 == 5) << endl;
-    cout << (3 > 5) << endl;
+    cout << !(3 < 5) << endl;
     return 0;
 }
